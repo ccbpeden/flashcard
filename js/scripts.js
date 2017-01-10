124c951c2762538d8c214eb11f40a067c19704bf
Charles Peden and Maggie Harrington <charles@>
2017-01-10 14:54:14 -0800

    clean html, styles.css, scripts.js for obsolete codes

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,4 @@
 $(document).ready(function() {
-  // $("#javaScripts").click(function(){
-    // $("#javaScripts").toggle();
-    // $("#javaScripth").toggle();
-  // });
   $("p.defAppear").click(function() {
     $(this).nextUntil("p.defAppear", "p.defInitHidden").toggleClass("defHidden");
   });
