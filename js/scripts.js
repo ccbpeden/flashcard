c0b7ac74bbf489d075542fed863bfb49cd28fc88
Charles Peden and Maggie Harrington <maggie@>
2017-01-10 14:21:32 -0800

    add class toggle functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
-  $("#javaScripts").click(function(){
-    $("#javaScripts").toggle();
-    $("#javaScripth").toggle();
+  // $("#javaScripts").click(function(){
+    // $("#javaScripts").toggle();
+    // $("#javaScripth").toggle();
+  // });
+  $("p.defAppear").click(function() {
+    $(this).nextUntil("p.defAppear", "p.defInitHidden").toggleClass("defHidden");
   });
-};
+});
